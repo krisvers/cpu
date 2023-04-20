6e7e83f9dd01246d9d6d62e2a455cfe8eac81a48
Nicholas Kelly - Student <kellynicholasi@lexington1.net>
2023-04-20 00:28:17 +0000

    Initial commit

---
+++ isa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+
+#define CODE_LENGTH 256
+
+uint8_t memory[8192];
+
+uint8_t code[CODE_LENGTH] = {
+	
+};
+
+int main() {
+	memcpy(memory, code, CODE_LENGHT);
+
+	while (1) {
+		
+	}
+}
