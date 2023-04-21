cc8ea0b0fcfa3aeeac260976b2e5942f334e77e5
krisvers <nakoidevn@gmail.com>
2023-04-20 22:05:08 -0400

    Intermediate

--- cpu.c
+++ cpu.c
@@ -4,6 +4,22 @@
 #include <stdio.h>
 
 #define MEMORY_SIZE 8192
+#define NOP 0b0000000
+#define LOD 0b0000001
+#define STR 0b0000010
+#define ADD 0b0000011
+#define SUB 0b0000100
+#define MUL 0b0000101
+#define DIV 0b0000110
+#define REM 0b0000111
+#define OR  0b0001000
+#define AND 0b0001001
+#define XOR 0b0001010
+#define NOT 0b0001011
+#define SHR 0b0001100
+#define SHL 0b0001101
+#define PSH 0b0001110
+#define JMP 0b0001111
 
 #define CPU_REGISTER_INDEX(n) \
 	(*(((uint32_t *) &reg) + n))
@@ -27,7 +43,7 @@ struct cpuInstruction cpu_fetch_instruction() {
 }
 
 int cpu_run_instruction(struct cpuInstruction instruction) {
-	switch (instruction.instr) {
+	switch (instructn.instr) {
 		// nop
 		case 0b0000000:
 			return 0;
@@ -52,7 +68,10 @@ int cpu_run_instruction(struct cpuInstruction instruction) {
 			if (instruction.mod) {
 				
 			}
+
 			// store reg, reg
+
+			return 0;
 		default:
 			return -2;
 	}
